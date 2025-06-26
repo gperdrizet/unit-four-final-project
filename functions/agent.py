060a8f4a56708818c328ac2d41574ccdc1f35aa0
George Perdrizet <george@perdrizet.org>
2025-06-26 16:51:45 -0400

    Split tools up amoung wikipedia agent and websearch agent and implementend manager agent, all using qwen2.5-coder-32B.

--- functions/agent.py
+++ functions/agent.py
@@ -1,7 +1,7 @@
 '''Agent definition for GAIA question answering system.'''
 
 # Imports for agent creation
-from smolagents import CodeAgent, InferenceClientModel, VisitWebpageTool
+from smolagents import CodeAgent, ToolCallingAgent, InferenceClientModel, VisitWebpageTool
 from functions.tools import (
     google_search,
     wikipedia_search,
@@ -12,26 +12,50 @@ def create_agent():
     '''Creates agent for GAIA question answering system.'''
 
     model = InferenceClientModel(
-        # max_tokens=8096,
-        # temperature=0.5,
-        model_id='Qwen/Qwen2.5-Coder-32B-Instruct',
-        provider='together'
-        # custom_role_conversions=None
+        "Qwen/Qwen2.5-Coder-32B-Instruct", provider="together"
     )
 
-    tools = [
+    web_agent_tools = [
         google_search,
-        wikipedia_search,
-        get_wikipedia_page,
         VisitWebpageTool()
     ]
 
-    agent = CodeAgent(
-        tools=tools,
+    web_agent = CodeAgent(
         model=model,
-        max_steps=20,
-        planning_interval=2,
-        additional_authorized_imports=['bs4.*']
+        tools=web_agent_tools,
+        name="web_agent",
+        description="Browses the web to find information",
+        verbosity_level=1,
+        max_steps=10,
     )
 
-    return agent
+    wikipedia_agent_tools = [
+        wikipedia_search,
+        get_wikipedia_page
+    ]
+
+    wikipedia_agent = CodeAgent(
+        model=model,
+        tools=wikipedia_agent_tools,
+        additional_authorized_imports=['bs4.*'],
+        name="wikipedia_agent",
+        description="Search Wikipedia and retrieve pages",
+        verbosity_level=1,
+        max_steps=10
+    )
+
+    manager_agent = CodeAgent(
+        model=model,
+        tools=[],
+        additional_authorized_imports=['bs4.*'],
+        name="manager_agent",
+        description="Manages the workflow of other agents",
+        managed_agents=[web_agent, wikipedia_agent],
+        planning_interval=1,
+        verbosity_level=2,
+        max_steps=15,
+    )
+
+    manager_agent.visualize()
+
+    return manager_agent
